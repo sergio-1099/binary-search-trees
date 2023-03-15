e4560b6477904aa92e308b085fee3b6c2068caf4
Sergio Guerra <sergioguerra6188@gmail.com>
2023-03-15 12:32:19 -0400

    Implement #pretty_print method to visualize data
    Uses code found from The Odin Project. Project: Binary Search Trees.

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -36,4 +36,10 @@ class Tree
     end
     return root
   end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right_node, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right_node
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.value}"
+    pretty_print(node.left_node, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left_node
+  end
 end
