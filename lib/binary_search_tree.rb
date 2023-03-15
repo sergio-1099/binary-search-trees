da4fc36b36d397916a07f11bf11268c6b507916e
Sergio Guerra <sergioguerra6188@gmail.com>
2023-03-15 12:23:19 -0400

    Implement #build_tree(array) method
    Uses recursion to build nodes from a sorted array. Recursion allows for
    code to be reused as trees are recursive. A left node of the root is a
    subtree and a subproblem of the whole tree.

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -1,3 +1,4 @@
+require 'pry-byebug'
 class Node
   include Comparable
   attr_accessor :value, :left_node, :right_node
@@ -15,6 +16,24 @@ end
 
 class Tree
   def initialize(array)
-    @root = self.build_tree
+    array = array.sort
+    @root = self.build_tree(array)
+  end
+
+  def build_tree(array)
+    root = nil
+    if (array.size == 1)
+      leaf = Node.new
+      leaf.value = array[0]
+      return leaf
+    elsif (array == [])
+      return nil
+    else 
+      root = Node.new
+      root.value = array[(array.size)/2]
+      root.left_node = self.build_tree(array[0..array.size/2-1])
+      root.right_node = self.build_tree(array[array.size/2+1..array.size-1])
+    end
+    return root
   end
 end
