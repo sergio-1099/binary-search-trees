1bd47af123a066cc69bbdafc56d8a22a64f8b7bc
Sergio Guerra <sergioguerra6188@gmail.com>
2023-03-24 10:51:25 -0400

    Add driver.rb to test Tree class

---
+++ lib/driver.rb
@@ -0,0 +1,42 @@
+require_relative('binary_search_tree')
+require 'pry-byebug'
+
+array = (Array.new(15) { rand(1..100) })
+tree = Tree.new(array)
+
+tree.pretty_print
+puts "That the tree is balanced is #{tree.balanced?}"
+
+print "Level Order: "
+tree.level_order { |node| print "#{node.value} " }
+print "\nPreorder: "
+tree.preorder { |node| print "#{node.value} "}
+print "\nPostorder: "
+tree.postorder { |node | print "#{node.value} "}
+print "\nInorder: "
+tree.inorder { |node| print "#{node.value} " }
+
+puts "\nUnbalancing..."
+tree.insert(101)
+tree.insert(200)
+tree.insert(155)
+tree.insert(132)
+
+tree.pretty_print
+
+binding.pry
+puts "That the tree is balanced is #{tree.balanced?}"
+
+puts "\nRebalancing..."
+tree.rebalance!
+
+puts "That the tree is balanced is #{tree.balanced?}"
+
+print "Level Order: "
+tree.level_order { |node| print "#{node.value} " }
+print "\nPreorder: "
+tree.preorder { |node| print "#{node.value} "}
+print "\nPostorder: "
+tree.postorder { |node | print "#{node.value} "}
+print "\nInorder: "
+tree.inorder { |node| print "#{node.value} " }
